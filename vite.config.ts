47a1f2b3c73190eb6318fc10b7d049a8341ff2e2
Marty Cox <marty.cox@amber.com.au>
2026-04-05 22:09:53 +0100

    more tidy up and custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/marty-lab/',
   plugins: [react()],
   css: {
     modules: {
